4ce761df9ebec821b47ad5a3e9fcbccda03f7a6f
Eric Berquist <eric.berquist@gmail.com>
2018-07-20 16:55:01 -0400

    pylint

--- test/test_parse_matrices_dalton.py
+++ test/test_parse_matrices_dalton.py
@@ -9,7 +9,7 @@ from pyints import parse_matrices_dalton
 
 
 __filedir__ = os.path.realpath(os.path.dirname(__file__))
-refdir = os.path.join(__filedir__, 'reference_data')
+__refdir__ = os.path.join(__filedir__, 'reference_data')
 
 
 def test_parse_element_dalton():
@@ -29,11 +29,10 @@ def test_parse_element_dalton():
 
 def test_parse_matrix_dalton():
     """Test parsing a single sparse matrix from a DALTON output file."""
-    # outputfilename = os.path.join(refdir, 'LiH_STO-3G', 'LiH_STO-3G_dalton.out')
     testcase = 'LiH_STO-3G'
     stub = 'dalton_matrix'
-    sourcefilename = os.path.join(refdir, testcase, '{stub}.dat'.format(stub=stub))
-    reffilename = os.path.join(refdir, testcase, '{stub}.yaml'.format(stub=stub))
+    sourcefilename = os.path.join(__refdir__, testcase, '{stub}.dat'.format(stub=stub))
+    reffilename = os.path.join(__refdir__, testcase, '{stub}.yaml'.format(stub=stub))
     assert os.path.exists(sourcefilename)
     with open(sourcefilename) as sourcefile:
         res_spmat = parse_matrices_dalton.parse_matrix_dalton(sourcefile)
@@ -43,6 +42,7 @@ def test_parse_matrix_dalton():
     assert res_spmat == ref_spmat
 
 
+# pylint: disable=invalid-name
 def test_sparse_to_dense_matrix_dalton():
     """Test converting a sparse (dictionary of dictionary of floats)
     matrix to a dense (NumPy) matrix.
@@ -50,8 +50,8 @@ def test_sparse_to_dense_matrix_dalton():
     testcase = 'LiH_STO-3G'
     stub_source = 'dalton_matrix'
     stub_ref = 'dalton_matrix'
-    sourcefilename = os.path.join(refdir, testcase, '{stub}.yaml'.format(stub=stub_source))
-    reffilename = os.path.join(refdir, testcase, '{stub}.npy'.format(stub=stub_ref))
+    sourcefilename = os.path.join(__refdir__, testcase, '{stub}.yaml'.format(stub=stub_source))
+    reffilename = os.path.join(__refdir__, testcase, '{stub}.npy'.format(stub=stub_ref))
     assert os.path.exists(sourcefilename)
     with open(sourcefilename) as sourcefile:
         spmat = yaml.load(sourcefile)
@@ -63,16 +63,19 @@ def test_sparse_to_dense_matrix_dalton():
     np.testing.assert_allclose(res_mat, ref_mat, rtol=0, atol=1.0e-14)
 
 
+# pylint: disable=too-many-locals
 def test_parse_spin_orbit_2el():
+    """Test reading the two-electron spin-orbit integrals from a
+    (reversed) DALTON text output file.
+    """
     testcase = 'LiH_STO-3G'
-    stub_sourcefile = 'dalton_spin_orbit_2el_reversed'
-    stub_reffile = 'dalton_spin_orbit_2el'
-    sourcefilename = os.path.join(refdir, testcase, '{stub}.dat'.format(stub=stub_sourcefile))
-    reffilename = os.path.join(refdir, testcase, '{stub}.yaml'.format(stub=stub_reffile))
+    stub_source = 'dalton_spin_orbit_2el_reversed'
+    stub_ref = 'dalton_spin_orbit_2el'
+    sourcefilename = os.path.join(__refdir__, testcase, '{stub}.dat'.format(stub=stub_source))
+    reffilename = os.path.join(__refdir__, testcase, '{stub}.yaml'.format(stub=stub_ref))
     with open(sourcefilename) as sourcefile:
         for line in sourcefile:
             if 'spin-orbit two-electron integrals and' in line:
-                # nlines = int(line.split()[0])
                 while '##' not in line:
                     line = next(sourcefile)
                 # Assume that the largest index printed is the
@@ -88,6 +91,7 @@ def test_parse_spin_orbit_2el():
         indices = tuple(np.array(indices, dtype=int) - 1)
         assert res_so2el[coord][indices] == val
     # Make sure the elements above are the only ones that are nonzero.
+    # pylint: disable=invalid-name
     x, y, z = res_so2el
     l = x[np.abs(x) > 0].tolist() \
         + y[np.abs(y) > 0].tolist() \
